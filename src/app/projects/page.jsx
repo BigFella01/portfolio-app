706b233d56e94ec89d5e235cdad55d2764dc786f
BigFella01 <112123830+BigFella01@users.noreply.github.com>
2024-10-21 21:16:06 -0400

    Update page.jsx

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -2,6 +2,7 @@ import { Card, CardBody, CardHeader, Image, Link } from "@nextui-org/react";
 import { FaHtml5 } from "react-icons/fa";
 import { FaCss3Alt } from "react-icons/fa";
 import { IoLogoJavascript } from "react-icons/io5";
+import { BiLogoTypescript } from "react-icons/bi";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { FaReact } from "react-icons/fa";
 import { RiNextjsFill } from "react-icons/ri";
@@ -19,21 +20,24 @@ export default function ProjectsPage() {
               href="https://cruise-control-psi.vercel.app"
               isExternal={true}
             >
-              <Image alt="preview for project" src="/images/cruise-control.png" />
+              <Image
+                alt="preview for project"
+                src="/images/cruise-control.png"
+              />
             </Link>
           </CardBody>
           <CardHeader className="grid text-center">
             <h4 className="text-purple-300">Cruise Control</h4>
-            <div class="my-4 border-t border-gray-700"></div>
+            <div className="my-4 border-t border-gray-700"></div>
             <p>
               Full-stack web application that allows users to create bicycle
               rentals with a hypothetical business based out of Pensacola, FL.
             </p>
-            <div class="my-4 border-t border-gray-700"></div>
+            <div className="my-4 border-t border-gray-700"></div>
             <div className="flex flex-wrap gap-2 justify-center">
               <FaHtml5 className={styles.iconSmall} />
               <FaCss3Alt className={styles.iconSmall} />
-              <IoLogoJavascript className={styles.iconSmall} />
+              <BiLogoTypescript className={styles.iconSmall} />
               <FaReact className={styles.iconSmall} />
               <SiPostgresql className={styles.iconSmall} />
             </div>
@@ -42,12 +46,15 @@ export default function ProjectsPage() {
         <Card className="bg-black text-white p-8 border rounded-xl border-purple-500">
           <CardBody>
             <Link href="https://purepalate2-0-psi.vercel.app" isExternal={true}>
-              <Image alt="preview for project" src="/images/purepalate2.0.png" />
+              <Image
+                alt="preview for project"
+                src="/images/purepalate2.0.png"
+              />
             </Link>
           </CardBody>
           <CardHeader className="grid text-center">
             <h4 className="text-purple-300">Pure Palate 2.0</h4>
-            <div class="my-4 border-t border-gray-700"></div>
+            <div className="my-4 border-t border-gray-700"></div>
             <p>
               Full-stack web application with thousands of recipes with
               comprehensive nutrition information. App also features a 'Create
@@ -58,7 +65,7 @@ export default function ProjectsPage() {
             <div className="flex flex-wrap gap-2 justify-center">
               <FaHtml5 className={styles.iconSmall} />
               <FaCss3Alt className={styles.iconSmall} />
-              <IoLogoJavascript className={styles.iconSmall} />
+              <BiLogoTypescript className={styles.iconSmall} />
               <RiNextjsFill className={styles.iconSmall} />
               <RiTailwindCssFill className={styles.iconSmall} />
               <SiPostgresql className={styles.iconSmall} />
@@ -77,7 +84,7 @@ export default function ProjectsPage() {
           </CardBody>
           <CardHeader className="grid text-center">
             <h4 className="text-purple-300">Discuss</h4>
-            <div class="my-4 border-t border-gray-700"></div>
+            <div className="my-4 border-t border-gray-700"></div>
             <p>
               Full-stack blog website. Gives users the ability to start
               conversations about anything and everything. App is build with
@@ -98,3 +105,4 @@ export default function ProjectsPage() {
     </div>
   );
 }
+
